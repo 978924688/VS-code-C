3db15e279c3cd976742ec84a8ccd2cf163b03f04
978924688@qq.com <978924688@qq.com>
2020-03-21 18:22:17 +0800

    C vscode demo

--- HelloWorld.c
+++ HelloWorld.c
@@ -1,9 +1,23 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <iostream.h>
+
+
+int temp = 0;
+
 
 int main(void)
 {
+    int j;
+
+    for(int i=0;i<5;i++)
+    {
+        temp++;
+        printf("i=%d\n",i);
+    }
+
     printf("Hello World\n");
+    cout <<"Hello World");
     system("pause");
     return 0;
 }
